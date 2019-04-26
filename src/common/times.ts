9d878e3bb501a67d5d0201f2c1aee6d87a8c1a77
Mike Burton <mbrtn@icloud.com>
2019-04-26 17:42:35 +0300

    Update

--- src/common/times.ts
+++ src/common/times.ts
@@ -1,9 +1,7 @@
 export enum ETimesPer {
   Day,
-  Week,
 }
 
 export const timesPerNames = new Map<ETimesPer, string>();
 
 timesPerNames.set(ETimesPer.Day, 'TIMES_PER.DAY');
-timesPerNames.set(ETimesPer.Week, 'TIMES_PER.WEEK');
